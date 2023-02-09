01219e267e915ec52be75857df2f3428934de081
Anatalia Neves Mula <mulaanatalia@gmail.com>
2023-02-09 02:27:24 +0200

    ui(landing): integração do template

---
+++ resources/views/landing/header.blade.php
@@ -0,0 +1,18 @@
+<header class="site-header">
+            <div class="container">
+                <div class="site-header-inner">
+                    <div class="brand header-brand">
+                        <h1 class="m-0">
+							<a href="#">
+								<img 
+                                class="header-logo-image" 
+                                src="{{ asset('contents/logo.jpg') }}" 
+                                alt="Logo"
+                                style="width: 50px; height: 50px;" 
+                                >
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </header>
