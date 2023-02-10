74de00fcf8960d2ee56cf9d412ac8cc48714dc42
Anatalia Neves Mula <mulaanatalia@gmail.com>
2023-02-10 15:13:51 +0200

    chore: rotas de API

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function getData(){
+        return response()->json(['requisicao realizada com sucesso...']);
+    }
 }
